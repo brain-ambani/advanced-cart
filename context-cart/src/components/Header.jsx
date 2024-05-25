a27053fead6a1dc1c0115e58b92cd29375bdf422
brian-ambani <brainambani1@gmail.com>
2024-05-26 01:10:15 +0300

    updated the context-cart

--- context-cart/src/components/Header.jsx
+++ context-cart/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   const { data } = useContext(CartContext);
   return (
     <nav className=" shadow-md p-4 fixed w-full bg-slate-800">
-      <div className="w-[80%] mx-auto pt-4 ">
+      <div className="w-[80%] mx-auto pt-4 top-0">
         <ul className="flex justify-between text-xl  uppercase ">
           <li>
             <Link to="/">Home</Link>
--- context-cart/src/pages/Cart.jsx
+++ context-cart/src/pages/Cart.jsx
@@ -5,14 +5,26 @@ export default function Cart() {
   const { data } = useContext(CartContext);
 
   return (
-    <div>
-      <h1>Cart</h1>
-      {data.map((data, index) => (
-        <div key={index}>
-          <p>{data.name}</p>
-          <p>{data.price}</p>
+    <div className="w-3/4 mx-auto pt-24">
+      <h1 className="text-2xl mb-6 text-center">Cart</h1>
+
+      <div className="flex justify-between items-center ">
+        <div>
+          {data.map((data, index) => (
+            <div
+              key={index}
+              className="bg-slate-900 mb-2 w-[350px] rounded-md shadow-md p-2 flex justify-between"
+            >
+              <div className=" gap-4">
+                <p>{data.name}</p>
+                <p>{data.price}</p>
+              </div>
+              <button className="text-red-700 font-semibold ">X</button>
+            </div>
+          ))}
         </div>
-      ))}
+        <div className="">Stripe Checkout</div>
+      </div>
     </div>
   );
 }
