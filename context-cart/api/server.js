a162b7a6d3ffa439194c450f8c7e3cac119551c9
brian-ambani <brainambani1@gmail.com>
2024-05-26 10:25:48 +0300

    updated the context cart with stripe

---
+++ context-cart/api/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const Stripe = require('stripe')
+
+const stripe = Stripe()
+
+const router = express.Router()
+
+router.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: "{{PRICE_ID}}",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.send(url: session.url);
+});
+
+module.exports = router
